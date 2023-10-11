daa61b34b87a52fe8600aac6bf21671f086b9fa9
Glen907 <144655536+Glen907@users.noreply.github.com>
2023-10-11 09:50:28 +0300

    Add files via upload

---
+++ задание5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale (LC_ALL, "RU");
+    cout<<"Равнобедренный треугольник из символов *\n";
+    cout<<"    *\n";
+    cout<<"   ***\n";
+    cout<<"  *****\n";
+    cout<<" *******\n";
+    cout<<"*********\n";
+    return 0;
+}
